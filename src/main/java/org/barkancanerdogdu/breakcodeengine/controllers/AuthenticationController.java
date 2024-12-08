a7a9f06be0a252281bf87572aac1c65109eb7305
BCE <bce@192.168.1.107>
2024-12-09 00:41:13 +0300

    controllers and login page

---
+++ src/main/java/org/barkancanerdogdu/breakcodeengine/controllers/AuthenticationController.java
@@ -0,0 +1,28 @@
+package org.barkancanerdogdu.breakcodeengine.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/login")
+public class AuthenticationController {
+
+    @GetMapping
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @PostMapping("/auth")
+    public String handleLogin(@RequestParam String username, @RequestParam String password, Model model) {
+        if ("admin".equals(username) && "password".equals(password)) {
+            return "redirect:/dashboard";
+        } else {
+            model.addAttribute("error", "Invalid username or password");
+            return "login";
+        }
+    }
+}

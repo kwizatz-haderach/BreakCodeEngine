ad6db9f5cd214c272cae61f88e3f27f9f42cfb8d
BCE <bce@BCEs-MacBook-Air.local>
2024-12-08 21:39:20 +0300

    first commit

---
+++ src/main/java/org/barkancanerdogdu/breakcodeengine/BreakCodeEngineApplication.java
@@ -0,0 +1,13 @@
+package org.barkancanerdogdu.breakcodeengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakCodeEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreakCodeEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/barkancanerdogdu/breakcodeengine/BreakCodeEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.barkancanerdogdu.breakcodeengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakCodeEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
